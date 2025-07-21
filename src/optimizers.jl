e303697eaff6557a058554ce4fb5ba8bb0d506a9
Yichao Yu <yyc1992@gmail.com>
2025-07-21 10:35:03 -0400

    Add generic objective for pre-optimization of solutions

--- src/optimizers.jl
+++ src/optimizers.jl
@@ -3,6 +3,11 @@
 module Optimizers
 
 import ForwardDiff
+using StaticArrays
+
+import ..Sequence as Seq
+import ..SymLinear as SL
+import ..SegSeq as SS
 
 struct NLVarTracker
     vars::Vector{Tuple{Float64,Float64}}
@@ -48,6 +53,67 @@ function autodiff(f::F) where F
         end
         return f(x)
     end
+end
+
+struct AbsAreaObjCallback{NModes}
+    dis_weights::MVector{NModes,Float64}
+    disδ_weights::MVector{NModes,Float64}
+    area_weights::MVector{NModes,Float64}
+end
+
+function (obj::AbsAreaObjCallback{NModes})(vals, grads) where NModes
+    @assert length(vals) == 3 * NModes
+    @assert length(grads) == 3 * NModes
+    v1 = zero(eltype(vals))
+    v2 = zero(eltype(vals))
+    @inbounds @simd ivdep for i in 1:NModes
+        v1 = muladd(vals[i], obj.dis_weights[i],
+                    muladd(vals[i + NModes], obj.disδ_weights[i], v1))
+        v2 = muladd(abs(vals[i + NModes * 2]), obj.area_weights[i], v2)
+    end
+    iv2 = 1 / v2
+
+    res = v1 * iv2
+    dv1 = iv2
+    dv2 = -res * iv2
+
+    @inbounds @simd ivdep for i in 1:NModes
+        grads[i] = obj.dis_weights[i] * dv1
+        grads[i + NModes] = obj.disδ_weights[i] * dv1
+        grads[i + NModes * 2] = flipsign(obj.area_weights[i] * dv2, vals[i + NModes * 2])
+    end
+    return res
+end
+
+function _init_weights!(weights, input)
+    if input === nothing
+        weights .= 1
+    else
+        weights .= input
+    end
+    return
+end
+
+function abs_area_obj(nseg, modes, pmask;
+                      freq=Seq.FreqSpec(), amp=Seq.AmpSpec(),
+                      dis_weights=nothing, disδ_weights=nothing, area_weights=nothing)
+    nmodes = length(modes.modes)
+    dis_args = ntuple(i->(:dis2, i), nmodes)
+    disδ_args = ntuple(i->(:disδ2, i), nmodes)
+    area_args = ntuple(i->(:area, i), nmodes)
+
+    cb = AbsAreaObjCallback{nmodes}(MVector{nmodes,Float64}(undef),
+                                    MVector{nmodes,Float64}(undef),
+                                    MVector{nmodes,Float64}(undef))
+    _init_weights!(cb.dis_weights, dis_weights)
+    _init_weights!(cb.disδ_weights, disδ_weights)
+    _init_weights!(cb.area_weights, area_weights)
+
+    mask_dis_area = SS.ValueMask(true, true, true, false, true, false)
+    buf = SL.ComputeBuffer{nseg,Float64}(Val(mask_dis_area), Val(mask_dis_area))
+
+    return Seq.Objective(pmask, (dis_args..., disδ_args..., area_args...),
+                         cb, modes, buf, freq=freq, amp=amp)
 end
 
 end
--- test/optimizers.jl
+++ test/optimizers.jl
@@ -2,8 +2,16 @@
 
 using Test
 
-using MSSim
-const Opts = MSSim.Optimizers
+using LinearAlgebra
+
+import MSSim.Optimizers as Opts
+import MSSim.SymLinear as SL
+import MSSim.Sequence as Seq
+
+function compute_grad(v₋₄, v₋₃, v₋₂, v₋₁, v₁, v₂, v₃, v₄, h)
+    return (-(v₄ - v₋₄) / 280 + 4 * (v₃ - v₋₃) / 105
+            - (v₂ - v₋₂) / 5 + 4 * (v₁ - v₋₁) / 5) / h
+end
 
 @testset "NLVarTracker" begin
     tracker = Opts.NLVarTracker(10)
@@ -45,5 +53,63 @@ end
         gs = zeros(3)
         @test func_diff(xs, gs) == func(xs)
         @test gs ≈ [xs[2], xs[1], 2]
+    end
+end
+
+@testset "Abs Area Objective" begin
+    modes = Seq.Modes()
+    for i in 1:5
+        push!(modes, (2.1 + 0.1 * i) * 2π)
+    end
+    obj = Opts.abs_area_obj(10, modes, SL.pmask_tfm)
+    @test obj.obj.dis_weights == [1, 1, 1, 1, 1]
+    @test obj.obj.disδ_weights == [1, 1, 1, 1, 1]
+    @test obj.obj.area_weights == [1, 1, 1, 1, 1]
+
+    obj = Opts.abs_area_obj(10, modes, SL.pmask_tfm,
+                            dis_weights=2, disδ_weights=0.1, area_weights=0.3)
+    @test obj.obj.dis_weights == [2, 2, 2, 2, 2]
+    @test obj.obj.disδ_weights == [0.1, 0.1, 0.1, 0.1, 0.1]
+    @test obj.obj.area_weights == [0.3, 0.3, 0.3, 0.3, 0.3]
+
+    dis_weights = rand(5)
+    disδ_weights = rand(5)
+    area_weights = rand(5)
+    obj = Opts.abs_area_obj(10, modes, SL.pmask_tfm,
+                            dis_weights=dis_weights, disδ_weights=disδ_weights,
+                            area_weights=area_weights)
+    @test obj.obj.dis_weights == dis_weights
+    @test obj.obj.disδ_weights == disδ_weights
+    @test obj.obj.area_weights == area_weights
+
+    args = Vector{Float64}(undef, 15)
+    args2 = Vector{Float64}(undef, 15)
+    grads = Vector{Float64}(undef, 15)
+    grads2 = Vector{Float64}(undef, 15)
+    for _ in 1:1000
+        dis = rand(5)
+        disδ = rand(5)
+        area = (rand(5) .+ 0.1) .* rand((1, -1), 5)
+
+        args[1:5] .= dis
+        args[6:10] .= disδ
+        args[11:15] .= area
+        args2 .= args
+
+        function eval_wrapper(i, d)
+            args2[i] = args[i] + d
+            return obj.obj(args2, grads2)
+        end
+
+        @test obj.obj(args, grads) ≈
+            (dot(dis, dis_weights) + dot(disδ, disδ_weights)) / dot(abs.(area), area_weights)
+
+        h = 0.000005 / 4
+        hs = (-4, -3, -2, -1, 1, 2, 3, 4) .* h
+        for i in 1:15
+            results = eval_wrapper.(i, hs)
+            args2[i] = args[i]
+            @test grads[i] ≈ compute_grad(results..., h) rtol=1.5e-5 atol=5e-5
+        end
     end
 end
