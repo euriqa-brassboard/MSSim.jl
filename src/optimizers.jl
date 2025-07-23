f74606b2591522bd10221bf9610ee1326fb26566
Yichao Yu <yyc1992@gmail.com>
2025-07-23 14:37:10 -0400

    Objective function for final optimization

--- src/optimizers.jl
+++ src/optimizers.jl
@@ -137,4 +137,247 @@ function abs_area_obj(nseg, modes, pmask;
                          modes, buf, freq=freq, amp=amp)
 end
 
+struct AreaTarget
+    start_idx::Int
+    target::Float64
+    area_weights::Vector{Float64}
+    areaδ_weights::Vector{Float64}
+    function AreaTarget(start_idx; target=π / 2, area_weights, areaδ_weights=Float64[])
+        return new(start_idx, target, area_weights, areaδ_weights)
+    end
+end
+
+struct _AreaInfo
+    mode_start_idx::Int
+    nmodes::Int
+    target_idx::Int # area_weight index is target_idx + 1
+    has_areaδ::Bool
+end
+
+# AreaOffsets: tuple of _AreaInfo
+struct TargetAreaObjCallback{NP,NModes,AreaOffsets}
+    params::MVector{NP,Float64}
+    function TargetAreaObjCallback(NModes, dis_weights, disδ_weights, area_targets)
+        @assert NModes >= 1
+
+        params = Vector{Float64}(undef, 2 * NModes)
+        _init_weights!(@view(params[1:NModes]), dis_weights)
+        _init_weights!(@view(params[NModes + 1:2 * NModes]), disδ_weights)
+
+        area_info = _AreaInfo[]
+        area_idx_offset = 2 * NModes
+        for area_tgt::AreaTarget in area_targets
+            @assert area_tgt.start_idx <= NModes
+            area_nmodes = length(area_tgt.area_weights)
+            @assert area_tgt.start_idx + area_nmodes - 1 <= NModes
+            has_areaδ = !isempty(area_tgt.areaδ_weights)
+            target_idx = area_idx_offset + 1
+            push!(area_info, _AreaInfo(area_tgt.start_idx, area_nmodes,
+                                       target_idx, has_areaδ))
+            area_idx_offset += has_areaδ ? (2 * area_nmodes + 1) : (area_nmodes + 1)
+            resize!(params, area_idx_offset)
+            params[target_idx] = area_tgt.target
+            params[target_idx + 1:target_idx + area_nmodes] .= area_tgt.area_weights
+            if has_areaδ
+                @assert length(area_tgt.areaδ_weights) == area_nmodes
+                params[target_idx + 1 + area_nmodes:target_idx + 2 * area_nmodes] .=
+                    area_tgt.areaδ_weights
+            end
+        end
+        NP = length(params)
+        return new{NP,NModes,(area_info...,)}(params)
+    end
+end
+
+struct AreaTargetWrapper{Obj,AreaInfo}
+    _obj::Obj
+end
+@inline function Base.getproperty(wrapper::AreaTargetWrapper{Obj,AreaInfo},
+                                  field::Symbol) where {Obj,AreaInfo}
+    p = getfield(wrapper, :_obj).params
+    if field === :target
+        return p[AreaInfo.target_idx]
+    elseif field === :area_weights
+        return @view p[AreaInfo.target_idx + 1:AreaInfo.target_idx + AreaInfo.nmodes]
+    elseif field === :areaδ_weights
+        if AreaInfo.has_areaδ
+            return @view p[AreaInfo.target_idx + AreaInfo.nmodes + 1:AreaInfo.target_idx + AreaInfo.nmodes * 2]
+        else
+            return nothing
+        end
+    else
+        return getfield(wrapper, field)
+    end
+end
+@inline function Base.setproperty!(wrapper::AreaTargetWrapper{Obj,AreaInfo},
+                                   field::Symbol, value) where {Obj,AreaInfo}
+    p = getfield(wrapper, :_obj).params
+    if field === :target
+        p[AreaInfo.target_idx] = value
+    else
+        setfield!(wrapper, field, value)
+    end
+    return
+end
+
+@inline function Base.getproperty(obj::TargetAreaObjCallback{NP,NModes,AreaOffsets}, field::Symbol) where {NP,NModes,AreaOffsets}
+    p = getfield(obj, :params)
+    if field === :dis_weights
+        return @view p[1:NModes]
+    elseif field === :disδ_weights
+        return @view p[NModes + 1:2 * NModes]
+    elseif field === :area_targets
+        return ntuple(i->AreaTargetWrapper{typeof(obj),AreaOffsets[i]}(obj),
+                      length(AreaOffsets))
+    else
+        return getfield(obj, field)
+    end
+end
+
+@generated function (obj::TargetAreaObjCallback{NP,NModes,AreaOffsets})(vals, grads) where {NP,NModes,AreaOffsets}
+    func_ex = quote
+        @assert length(vals) == $(NModes * 4)
+        @assert length(grads) == $(NModes * 4)
+        params = getfield(obj, :params)
+        res = zero(eltype(vals))
+        @inbounds @simd ivdep for i in 1:$(2 * NModes)
+            res = muladd(vals[i], params[i], res)
+            grads[i] = params[i]
+        end
+    end
+    area_used = [Int[] for i in 1:NModes]
+    areaδ_used = [Int[] for i in 1:NModes]
+    for (info_i, area_info) in enumerate(AreaOffsets)
+        mode_start_idx = area_info.mode_start_idx
+        nmodes = area_info.nmodes
+        for mode_i in mode_start_idx:mode_start_idx + nmodes - 1
+            push!(area_used[mode_i], info_i)
+            if area_info.has_areaδ
+                push!(areaδ_used[mode_i], info_i)
+            end
+        end
+    end
+    for mode_i in 1:NModes
+        if isempty(area_used[mode_i])
+            push!(func_ex.args, :(@inbounds grads[$(mode_i + NModes * 2)] = 0))
+        end
+        if isempty(areaδ_used[mode_i])
+            push!(func_ex.args, :(@inbounds grads[$(mode_i + NModes * 3)] = 0))
+        end
+    end
+    for (info_i, area_info) in enumerate(AreaOffsets)
+        mode_start_idx = area_info.mode_start_idx
+        nmodes = area_info.nmodes
+        target_idx = area_info.target_idx
+        has_areaδ = area_info.has_areaδ
+        push!(func_ex.args, quote
+                  @inbounds begin
+                      area = U.mul(vals[$(NModes * 2 + mode_start_idx)],
+                                   params[$(target_idx + 1)])
+                      $(has_areaδ ? quote
+                            areaδ = U.mul(vals[$(NModes * 3 + mode_start_idx)],
+                                           params[$(target_idx + nmodes + 1)])
+                        end : nothing)
+                  end
+                  @inbounds @simd ivdep for i in 2:$nmodes
+                      area = muladd(vals[$(NModes * 2 + mode_start_idx - 1) + i],
+                                    params[$target_idx + i], area)
+                      $(has_areaδ ? quote
+                            areaδ = muladd(vals[$(NModes * 3 + mode_start_idx - 1) + i],
+                                            params[$(target_idx + nmodes) + i], areaδ)
+                        end : nothing)
+                  end
+                  area_err = abs(area) - @inbounds params[$target_idx]
+                  area_err2 = flipsign(area_err * 2, area)
+                  res = muladd(area_err, area_err, res)
+                  $(has_areaδ ? quote
+                        areaδ2 = areaδ * 2
+                        res = muladd(areaδ, areaδ, res)
+                    end : nothing)
+              end)
+
+        area_all_first = true
+        areaδ_all_first = true
+        for mode_i in mode_start_idx:mode_start_idx + nmodes - 1
+            if area_used[mode_i][1] != info_i
+                area_all_first = false
+            end
+            if area_info.has_areaδ
+                if areaδ_used[mode_i][1] != info_i
+                    areaδ_all_first = false
+                end
+            end
+        end
+
+        if area_all_first
+            push!(func_ex.args, quote
+                      @inbounds @simd ivdep for i in 1:$nmodes
+                          grads[$(NModes * 2 + mode_start_idx - 1) + i] =
+                              params[$target_idx + i] * area_err2
+                      end
+                  end)
+        else
+            for mode_i in mode_start_idx:mode_start_idx + nmodes - 1
+                grad_idx = NModes * 2 + mode_i
+                param_idx = target_idx + 1 + mode_i - mode_start_idx
+                if area_used[mode_i][1] == info_i
+                    push!(func_ex.args,
+                          :(@inbounds(grads[$grad_idx] = params[$param_idx] * area_err2)))
+                else
+                    push!(func_ex.args,
+                          :(@inbounds(grads[$grad_idx] =
+                              muladd(params[$param_idx], area_err2, grads[$grad_idx]))))
+                end
+            end
+        end
+
+        if !has_areaδ
+            continue
+        end
+        if areaδ_all_first
+            push!(func_ex.args, quote
+                      @inbounds @simd ivdep for i in 1:$nmodes
+                          grads[$(NModes * 3 + mode_start_idx - 1) + i] =
+                              params[$(target_idx + nmodes) + i] * areaδ2
+                      end
+                  end)
+        else
+            for mode_i in mode_start_idx:mode_start_idx + nmodes - 1
+                grad_idx = NModes * 3 + mode_i
+                param_idx = target_idx + nmodes + 1 + mode_i - mode_start_idx
+                if area_used[mode_i][1] == info_i
+                    push!(func_ex.args,
+                          :(@inbounds(grads[$grad_idx] = params[$param_idx] * areaδ2)))
+                else
+                    push!(func_ex.args,
+                          :(@inbounds(grads[$grad_idx] =
+                              muladd(params[$param_idx], areaδ2, grads[$grad_idx]))))
+                end
+            end
+        end
+    end
+    push!(func_ex.args, quote
+              return res
+          end)
+    return func_ex
+end
+
+function target_area_obj(nseg, modes, pmask;
+                         freq=Seq.FreqSpec(), amp=Seq.AmpSpec(),
+                         dis_weights=nothing, disδ_weights=nothing, area_targets)
+    nmodes = length(modes.modes)
+    dis_args = ntuple(i->(:dis2, i), nmodes)
+    disδ_args = ntuple(i->(:disδ2, i), nmodes)
+    area_args = ntuple(i->(:area, i), nmodes)
+    areaδ_args = ntuple(i->(:areaδ, i), nmodes)
+
+    val_mask = SS.ValueMask(true, true, true, false, true, true)
+    buf = SL.ComputeBuffer{nseg,Float64}(Val(val_mask), Val(val_mask))
+
+    return Seq.Objective(pmask, (dis_args..., disδ_args..., area_args...),
+                         TargetAreaObjCallback(nmodes, dis_weights, disδ_weights,
+                                               area_targets),
+                         modes, buf, freq=freq, amp=amp)
+end
+
 end
--- test/optimizers.jl
+++ test/optimizers.jl
@@ -3,6 +3,7 @@
 using Test
 
 using LinearAlgebra
+using Random
 
 import MSSim.Optimizers as Opts
 import MSSim.SymLinear as SL
@@ -121,4 +122,141 @@ end
             end
         end
     end
+end
+
+@testset "Target Area Objective" begin
+    modes = Seq.Modes()
+    for i in 1:5
+        push!(modes, (2.1 + 0.1 * i) * 2π)
+    end
+    obj = Opts.target_area_obj(10, modes, SL.pmask_tfm, area_targets=Opts.AreaTarget[])
+    @test length(obj.obj.params) == 10
+    @test obj.obj.dis_weights == [1, 1, 1, 1, 1]
+    @test obj.obj.disδ_weights == [1, 1, 1, 1, 1]
+    @test obj.obj.area_targets == ()
+
+    obj = Opts.target_area_obj(10, modes, SL.pmask_tfm, area_targets=Opts.AreaTarget[],
+                               dis_weights=2, disδ_weights=0.1)
+    @test obj.obj.dis_weights == [2, 2, 2, 2, 2]
+    @test obj.obj.disδ_weights == [0.1, 0.1, 0.1, 0.1, 0.1]
+    @test obj.obj.area_targets == ()
+
+    dis_weights = rand(5)
+    disδ_weights = rand(5)
+    obj = Opts.target_area_obj(10, modes, SL.pmask_tfm,
+                               area_targets=[Opts.AreaTarget(1, target=0.1,
+                                                             area_weights=[0.1, -0.2])],
+                               dis_weights=dis_weights, disδ_weights=disδ_weights)
+    @test length(obj.obj.params) == 13
+    @test obj.obj.dis_weights == dis_weights
+    @test obj.obj.disδ_weights == disδ_weights
+    @test length(obj.obj.area_targets) == 1
+    area_tgt = obj.obj.area_targets[1]
+    @test area_tgt.target == 0.1
+    @test area_tgt.area_weights == [0.1, -0.2]
+    @test area_tgt.areaδ_weights === nothing
+    @test area_tgt._obj === obj.obj
+
+    area_tgt.target = 2.2
+    @test area_tgt.target == 2.2
+    area_tgt.area_weights .= [1.0, 3.0]
+    @test area_tgt.area_weights == [1.0, 3.0]
+
+    obj = Opts.target_area_obj(10, modes, SL.pmask_tfm,
+                               area_targets=[Opts.AreaTarget(1, target=0.1,
+                                                             area_weights=[0.2, -0.2]),
+                                             Opts.AreaTarget(2, target=0.9,
+                                                             area_weights=[0.5, -0.3],
+                                                             areaδ_weights=[0.8, -0.7])],
+                               dis_weights=dis_weights, disδ_weights=disδ_weights)
+    @test length(obj.obj.params) == 18
+    @test obj.obj.dis_weights == dis_weights
+    @test obj.obj.disδ_weights == disδ_weights
+    @test length(obj.obj.area_targets) == 2
+    area_tgt1 = obj.obj.area_targets[1]
+    @test area_tgt1.target == 0.1
+    @test area_tgt1.area_weights == [0.2, -0.2]
+    @test area_tgt1.areaδ_weights === nothing
+    @test area_tgt1._obj === obj.obj
+    area_tgt2 = obj.obj.area_targets[2]
+    @test area_tgt2.target == 0.9
+    @test area_tgt2.area_weights == [0.5, -0.3]
+    @test area_tgt2.areaδ_weights == [0.8, -0.7]
+    @test area_tgt2._obj === obj.obj
+
+    area_tgt1.target = 2.2
+    @test area_tgt1.target == 2.2
+    area_tgt1.area_weights .= [1.0, 3.0]
+    @test area_tgt1.area_weights == [1.0, 3.0]
+
+    area_tgt2.target = 2.9
+    @test area_tgt2.target == 2.9
+    area_tgt2.area_weights .= [-1.0, 3.9]
+    @test area_tgt2.area_weights == [-1.0, 3.9]
+    area_tgt2.areaδ_weights .= [2.9, 3.1]
+    @test area_tgt2.areaδ_weights == [2.9, 3.1]
+
+    function test_area_target(target_info)
+        area_targets = Opts.AreaTarget[]
+        nmodes = length(modes.modes)
+        nparams = nmodes * 2
+        dis_weights = rand(nmodes) .* 2 .- 1
+        disδ_weights = rand(nmodes) .* 2 .- 1
+        for (a_start, a_modes, hasδ) in target_info
+            push!(area_targets, Opts.AreaTarget(a_start, target=rand(),
+                                                area_weights=rand(a_modes) .* 2 .- 1,
+                                                areaδ_weights=hasδ ?
+                                                    rand(a_modes) .* 2 .- 1 :
+                                                    Float64[]))
+            nparams += 1 + (hasδ + 1) * a_modes
+        end
+        function obj_func(args)
+            dis = @view args[1:nmodes]
+            disδ = @view args[nmodes + 1:2 * nmodes]
+            area = @view args[2 * nmodes + 1:3 * nmodes]
+            areaδ = @view args[3 * nmodes + 1:4 * nmodes]
+            res = dot(dis, dis_weights) + dot(disδ, disδ_weights)
+            for area_tgt in area_targets
+                a_modes = length(area_tgt.area_weights)
+                start_idx = area_tgt.start_idx
+                res += (abs(dot(@view(area[start_idx:start_idx + a_modes - 1]),
+                                area_tgt.area_weights)) - area_tgt.target)^2
+                if !isempty(area_tgt.areaδ_weights)
+                    res += dot(@view(areaδ[start_idx:start_idx + a_modes - 1]),
+                               area_tgt.areaδ_weights)^2
+                end
+            end
+            return res
+        end
+        obj = Opts.target_area_obj(10, modes, SL.pmask_full,
+                                   dis_weights=dis_weights, disδ_weights=disδ_weights,
+                                   area_targets=area_targets).obj
+        @test length(obj.params) == nparams
+        args = Vector{Float64}(undef, nmodes * 4)
+        args2 = Vector{Float64}(undef, nmodes * 4)
+        grads = Vector{Float64}(undef, nmodes * 4)
+        grads2 = Vector{Float64}(undef, nmodes * 4)
+        function eval_wrapper(i, d)
+            args2[i] = args[i] + d
+            return obj(args2, grads2)
+        end
+        for _ in 1:100
+            rand!(args)
+            args .= args .* 2 .- 1
+            @test obj(args, grads) ≈ obj_func(args)
+
+            args2 .= args
+            h = 0.000005 / 4
+            hs = (-4, -3, -2, -1, 1, 2, 3, 4) .* h
+            for i in 1:(nmodes * 4)
+                results = eval_wrapper.(i, hs)
+                args2[i] = args[i]
+                @test grads[i] ≈ compute_grad(results..., h) rtol=1.5e-5 atol=5e-5
+            end
+        end
+    end
+    test_area_target([])
+    test_area_target([(1, 2, true), (3, 2, false)])
+    test_area_target([(1, 5, false), (2, 3, true)])
+    test_area_target([(1, 4, true), (2, 4, true), (1, 2, false)])
 end
