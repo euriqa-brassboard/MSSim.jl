72b2939e5b1eeb3419d523bb3422a449d3e28ba8
Yichao Yu <yyc1992@gmail.com>
2025-10-13 01:21:06 -0400

    Far detuned MS with multiple modes

--- src/far_detune.jl
+++ src/far_detune.jl
@@ -2,23 +2,58 @@
 
 module FarDetune
 
-@inline function enclosed_area_kernel(τ, Ω11, Ω12, Ω21, Ω22, δ, grad)
+using Static
+
+macro accum_grad(keep, tgt, g, w)
+    tgt = esc(tgt)
+    g = esc(g)
+    w = esc(w)
+    quote
+        if dynamic($(esc(keep)))
+            $tgt = muladd($g, $w, $tgt)
+        else
+            $tgt = $g * $w
+        end
+    end
+end
+
+@inline function enclosed_area_kernel(τ, Ω11, Ω12, Ω21, Ω22, δ, grad,
+                                      weight=static(true);
+                                      keep_grad=(τ=static(false), δ=static(false),
+                                                 Ω11=static(false), Ω12=static(false),
+                                                 Ω21=static(false), Ω22=static(false)))
     δ⁻¹ = 1 / δ
     δ⁻¹_12 = δ⁻¹ / 12
     A = muladd(2, Ω21, Ω22)
     B = muladd(2, Ω22, Ω21)
     grad_τ = (A * Ω11 + B * Ω12) * δ⁻¹_12
 
-    res = τ * grad_τ
+    res = τ * grad_τ * weight
 
     @inbounds if !isempty(grad)
-        τδ⁻¹_12 = τ * δ⁻¹_12
-        grad[1] = grad_τ
-        grad[2] = τδ⁻¹_12 * A
-        grad[3] = τδ⁻¹_12 * B
-        grad[4] = τδ⁻¹_12 * muladd(2, Ω11, Ω12)
-        grad[5] = τδ⁻¹_12 * muladd(2, Ω12, Ω11)
-        grad[6] = -res * δ⁻¹
+        w2 = τ * δ⁻¹_12 * weight
+        @accum_grad(keep_grad.τ, grad[1], grad_τ, weight)
+        @accum_grad(keep_grad.Ω11, grad[2], A, w2)
+        @accum_grad(keep_grad.Ω12, grad[3], B, w2)
+        @accum_grad(keep_grad.Ω21, grad[4], muladd(2, Ω11, Ω12), w2)
+        @accum_grad(keep_grad.Ω22, grad[5], muladd(2, Ω12, Ω11), w2)
+        @accum_grad(keep_grad.δ, grad[6], -res, δ⁻¹)
+    end
+    return res
+end
+
+@inline function enclosed_area_modes(τ, Ω11, Ω12, Ω21, Ω22, δ, ωs, weights, grad;
+                                     keep_grad=(τ=static(false), δ=static(false),
+                                                Ω11=static(false), Ω12=static(false),
+                                                Ω21=static(false), Ω22=static(false)))
+    keep_all_grad = (τ=static(true), δ=static(true), Ω11=static(true), Ω12=static(true),
+                     Ω21=static(true), Ω22=static(true))
+    nmodes = length(ωs)
+    res = @inbounds enclosed_area_kernel(τ, Ω11, Ω12, Ω21, Ω22, δ - ωs[1], grad,
+                                         weights[1]; keep_grad=keep_grad)
+    @inbounds for i in 2:nmodes
+        res += enclosed_area_kernel(τ, Ω11, Ω12, Ω21, Ω22, δ - ωs[i], grad,
+                                    weights[i]; keep_grad=keep_all_grad)
     end
     return res
 end
--- test/far_detune.jl
+++ test/far_detune.jl
@@ -15,8 +15,14 @@ function numeric_area(τ, Ω11, Ω12, Ω21, Ω22, δ; atol=1e-8, rtol=1e-8)
 end
 
 @testset "FarDetune" begin
+    ωs0 = zeros(1)
+    ωs0_2 = zeros(2)
+    weights0 = ones(1)
+    weights0_2 = ones(2)
+
     grad = zeros(6)
-    for _ in 1:1000
+    grad2 = zeros(6)
+    for _ in 1:200
         τ = rand()
         Ω11 = rand() - 0.5
         Ω12 = rand() - 0.5
@@ -41,6 +47,39 @@ end
         @test vs_grad ≈ vs
         for i in 1:6
             @test grad[i] ≈ vs_autodiff.partials[i]
+        end
+
+        vs2_grad = FD.enclosed_area_modes(τ, Ω11, Ω12, Ω21, Ω22, δ, ωs0, weights0, grad2)
+        @test vs2_grad ≈ vs
+        @test grad2 ≈ grad
+
+        vs2_grad = FD.enclosed_area_modes(τ, Ω11, Ω12, Ω21, Ω22, δ,
+                                          ωs0_2, weights0_2, grad2)
+        @test vs2_grad ≈ 2 * vs
+        @test grad2 ≈ 2 .* grad
+
+        ωs = rand(5) .- 0.8
+        weights = rand(5) .- 0.5
+        vs2 = 0.0
+        for (ω, weight) in zip(ωs, weights)
+            vs2 += FD.enclosed_area_kernel(τ, Ω11, Ω12, Ω21, Ω22, δ - ω, ()) * weight
+        end
+        vs3 = FD.enclosed_area_modes(τ, Ω11, Ω12, Ω21, Ω22, δ, ωs, weights, ())
+        @test vs3 ≈ vs2
+
+        vs2_autodiff = FD.enclosed_area_modes(
+            ForwardDiff.Dual(τ, (1.0, 0.0, 0.0, 0.0, 0.0, 0.0)),
+            ForwardDiff.Dual(Ω11, (0.0, 1.0, 0.0, 0.0, 0.0, 0.0)),
+            ForwardDiff.Dual(Ω12, (0.0, 0.0, 1.0, 0.0, 0.0, 0.0)),
+            ForwardDiff.Dual(Ω21, (0.0, 0.0, 0.0, 1.0, 0.0, 0.0)),
+            ForwardDiff.Dual(Ω22, (0.0, 0.0, 0.0, 0.0, 1.0, 0.0)),
+            ForwardDiff.Dual(δ, (0.0, 0.0, 0.0, 0.0, 0.0, 1.0)), ωs, weights, ())
+        @test vs2_autodiff.value ≈ vs2
+
+        vs2_grad = FD.enclosed_area_modes(τ, Ω11, Ω12, Ω21, Ω22, δ, ωs, weights, grad2)
+        @test vs2_grad ≈ vs2
+        for i in 1:6
+            @test grad2[i] ≈ vs2_autodiff.partials[i]
         end
     end
 end
